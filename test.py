f635413d1d7ebe64eacc25f33b056b4c62c8a5cd
Gen Kamita <gen-kamita@gmo.com>
2017-05-13 21:23:51 +0900

    changed name of file and some minor edits. 	renamed:    doc2vec-bloomberg.py -> doc2vec.py 	modified:   test.py

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
 # coding: utf-8
 import pandas as pd
-df = pd.read_csv("timestamp.txt",names="a")
-df2 = pd.to_datetime(df.a)
+
+# title
+df = pd.read_csv("title.txt",names="a")
+print df.a.shape
+print df.a
+
+# timestamp
+#df = pd.read_csv("timestamp.txt",names="a")
+#df2 = pd.to_datetime(df.a)
