31b8bd7e5638cf5dbe74ce5665eb7d70a1a7fb88
Gen Kamita <gen-kamita@gmo.jp>
2017-05-17 18:11:02 +0900

    added comandline args. fixed bug and made changes. 	modified:   clustering.py

--- clustering.py
+++ clustering.py
@@ -1,13 +1,10 @@
 
-# coding: utf-8
-
-# In[1]:
-
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 import pprint as pp
+import sys
 
 def corpusNumLines():
     num_lines_in_corpus = get_ipython().getoutput(u'wc -l $corpus_train_file | sed "s/$corpus_train_file//g;"')
@@ -18,21 +15,26 @@ def corpusNumLines():
 docvec_file = "docvec60000"
 docvec = np.load(docvec_file)
 corpus_train_file = "split60000aa"
-num_cluster = 3
-num_lines_in_corpus = corpusNumLines()
-num_randpick = 50
-dimensionality_reduction = 1
+num_cluster = int(sys.argv[1])
+num_randpick = int(sys.argv[2])
+dimensionality_reduction = int(sys.argv[3])
+num_init = int(sys.argv[4]) 
 
-if dimensionality_reduction is True:
-    reduced_data = PCA(n_components=2).fit_transform(docvec)
-    kmeans = KMeans(init='k-means++', n_clusters = num_cluster)
-    fit_reduced_data = kmeans.fit(reduced_data, n_init=20)
+num_lines_in_corpus = corpusNumLines()
+
+if dimensionality_reduction == 1:
+    print("dimensionality reduction before clustering")
+    docvec = PCA(n_components=2).fit_transform(docvec)
+    kmeans = KMeans(init='k-means++', n_clusters = num_cluster, n_init=num_init)
+    fit = kmeans.fit(docvec)
 else:
+    print("performing clustering of raw docvec")
     X = np.load(docvec_file)
-    kmeans = KMeans(n_clusters=num_cluster).fit(docve)
+    kmeans = KMeans(init='k-means++', n_clusters = num_cluster, n_init=num_init)
+    fit = kmeans.fit(docvec)
 
 news_randpick_index = np.random.randint(0,num_lines_in_corpus-1,num_randpick)
-news_randpick_label = fit_reduced_data.labels_[news_randpick_index]
+news_randpick_label = fit.labels_[news_randpick_index]
 # sort them according to label
 news_randpick_label, news_randpick_index = (list(t) for t in zip(*sorted(zip(news_randpick_label, news_randpick_index))))
 
@@ -42,24 +44,19 @@ for num in news_randpick_index:
     news_randpick_title.append(title)
 
 for num in range(num_cluster):
-    print("cluster " + str(num) + ": ---------------------------------------")
     count = sum(i == num for i in news_randpick_label)
-    pp.pprint(news_randpick_title[0:count])
+    if count > 2:
+        print("cluster " + str(num) + ": ---------------------------------------")
+        pp.pprint(news_randpick_title[0:count])
     news_randpick_title = news_randpick_title[count:]
 
-
-if dimensionality_reduction is True:
+if dimensionality_reduction == 1:
+    print("plotting clustering of dimensionality reduced data")
     for num in range(num_cluster):
-        condition = fit_reduced_data.labels_ == num
-        filtered_reduced_data_x = reduced_data[condition,0]
-        filtered_reduced_data_y = reduced_data[condition,1]
-        plt.plot(filtered_reduced_data_x, filtered_reduced_data_y, '.', markersize=0.5)
+        condition = fit.labels_ == num
+        filtered_docvec_x = docvec[condition,0]
+        filtered_docvec_y = docvec[condition,1]
+        plt.plot(filtered_docvec_x, filtered_docvec_y, '.', markersize=0.5)
 
-    plt.plot(reduced_data[news_randpick_index, 0], reduced_data[news_randpick_index, 1], 'k.')
+    plt.plot(docvec[news_randpick_index, 0], docvec[news_randpick_index, 1], 'k.')
     plt.show()
-
-
-# In[ ]:
-
-
-
