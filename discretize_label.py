86548928bf799b67f39062a5a2fb0bff05605ed9
Gen Kamita <gen-kamita@gmo.jp>
2017-05-22 13:47:26 +0900

    labels are now [0, 1, 2, ...] changed naming.

--- discretize_label.py
+++ discretize_label.py
@@ -1,9 +1,9 @@
 # coding: utf-8
 import numpy
 
-def descretise_list(list_to_label,Bin=5):
+def discretise_label(list_to_label,Bin=5):
     minimum = min(list_to_label)
     maximum = max(list_to_label)
     bins = numpy.linspace(minimum,maximum,Bin)
     digitized = numpy.digitize(list_to_label, bins)
-    return digitized
+    return digitized - 1
