55808e62c06bc1c249ad179a67321abed940324c
Gen Kamita <genkamita@gmail.com>
2017-05-23 23:28:02 +0900

    some not so important stuff. 	modified:   test.py

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
-# coding: utf-8
-import pandas as pd
-
-# title
-df = pd.read_csv("title.txt",names="a")
-print df.a.shape
-print df.a
-
-# timestamp
-#df = pd.read_csv("timestamp.txt",names="a")
-#df2 = pd.to_datetime(df.a)
+from sklearn.cluster import KMeans
+import numpy as np
+X = np.array([[1, 2], [1, 4], [1, 0],
+               [4, 2], [4, 4], [4, 0]])
+kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
+kmeans.labels_
+kmeans.predict([[0, 0], [4, 4]])
+print(kmeans.cluster_centers_)
