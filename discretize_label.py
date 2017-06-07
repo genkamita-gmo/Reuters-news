358ed1d0c7c1629fa3a1d8a5ccc5e0d030d68548
Gen Kamita <gen-kamita@gmo.jp>
2017-06-07 15:22:59 +0900

    moved file to liu-ml4j 	deleted:    discretize_label.py

--- discretize_label.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-import numpy
-from collections import Counter
-
-
-def discretise_label(list_to_label,bin_list):
-    digitized = numpy.digitize(list_to_label, bin_list)
-    print "bin: ", str(bin_list),"-> counts of elements after binning:" ounter(digitized).values()
-    return digitized - 1
